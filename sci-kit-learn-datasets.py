f1dfc2e138ca26ea97f7417f4cf917d922e92cca
JuanjoBaeza <juanjose.baeza@gmail.com>
2021-10-25 12:48:57 +0200

    updated ai scripts

--- sci-kit-learn-datasets.py
+++ sci-kit-learn-datasets.py
@@ -6,11 +6,11 @@ data = datasets.load_breast_cancer()
 # Read the DataFrame, first using the feature data
 df = pd.DataFrame(data.data, columns=data.feature_names)
 
-# Add a target column, and fill it with the target data
-df['target'] = data.target
+# Add a target column, and fill it with the target data, 0 será Benigno y 1 Maligno
+df['Mal/Ben'] = data.target
 
 # Show the first five rows
-print(df.head())
+print(df.head(50))
 
 # Store the feature data
 X = data.data
@@ -25,4 +25,4 @@ from sklearn.neighbors import KNeighborsClassifier
 logreg = KNeighborsClassifier(n_neighbors=6)
 logreg.fit(X_train, y_train)
 
-print(logreg.score(X_test, y_test))
+print(round(logreg.score(X_test, y_test),3), "% precisión predictiva.")
